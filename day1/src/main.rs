2bb73de5f13da177a8b770720cd493c1a7e3a13c
Chris Rice <chris@chrisrice.io>
2022-01-22 16:12:06 -0500

    Finish day1 part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -6,12 +6,15 @@ use std::fs::read_to_string;
 fn main() {
     let input = read_to_string("input.txt").unwrap();
     let mut floor : i32 = 0;
-    for c in input.chars() {
+    for (i, c) in input.char_indices() {
         match c {
             '(' => floor += 1,
             ')' => floor -= 1,
             _   => panic!("derp"),
         }
+        if floor < 0 {
+            println!("{}", i+1);
+            break;
+        }
     }
-    println!("{}", floor);
 }
