ed81af0a281ada1b63e7856ed0f3e4ce1637a910
Chris Rice <chris@chrisrice.io>
2022-01-22 16:33:41 -0500

    Split day1 into separate fns for part1 and part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,16 +5,33 @@ use std::fs::read_to_string;
 
 fn main() {
     let input = read_to_string("input.txt").unwrap();
-    let mut floor : i32 = 0;
+    println!("part1: {}", part1(&input));
+    println!("part2: {}", part2(&input));
+}
+
+fn part1(input: &str) -> i32 {
+    let mut floor = 0;
+    for c in input.chars() {
+        match c {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => panic!("unexpected input"),
+        }
+    }
+    floor
+}
+
+fn part2(input: &str) -> usize {
+    let mut floor = 0;
     for (i, c) in input.char_indices() {
         match c {
             '(' => floor += 1,
             ')' => floor -= 1,
-            _   => panic!("derp"),
+            _ => panic!("unexpected input"),
         }
         if floor < 0 {
-            println!("{}", i+1);
-            break;
+            return i + 1;
         }
     }
+    panic!("never went negative");
 }
