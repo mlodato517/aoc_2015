c53349ac843c6bba9ed04c397f49c22f514eca17
Chris Rice <chris@chrisrice.io>
2022-01-22 15:55:45 -0500

    Do part1 the first time

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,17 @@
+
+
+use std::fs::read_to_string;
+
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_to_string("input.txt").unwrap();
+    let mut floor : i32 = 0;
+    for c in input.chars() {
+        match c {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _   => panic!("derp"),
+        }
+    }
+    println!("{}", floor);
 }
