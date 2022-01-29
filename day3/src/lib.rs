013a4ba3b7d01484924faac6e7948aa17d157ec1
Mark Lodato <mlodato517@gmail.com>
2022-01-29 14:53:53 -0500

    day3 initial commit

---
+++ day3/src/lib.rs
@@ -0,0 +1,24 @@
+pub fn part1(input: &str) -> u32 {
+    unimplemented!()
+}
+
+pub fn part2(input: &str) -> u32 {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1_examples() {}
+
+    #[test]
+    fn part_1_mark_input() {}
+
+    #[test]
+    fn test_part_2_examples() {}
+
+    #[test]
+    fn part_2_mark_input() {}
+}
---
+++ day3/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let input = include_str!("../input.txt");
+    println!("part1: {}", day3::part1(input));
+    println!("part2: {}", day3::part2(input));
+}
