5bcfe68dea4e30cb5a5819d351ba4ef366974123
Mark Lodato <mlodato517@gmail.com>
2022-02-05 14:43:30 -0500

    Day 3 part 2
    We're going to play around with some refactoring!

--- day3/src/lib.rs
+++ day3/src/lib.rs
@@ -28,8 +28,57 @@ pub fn part1(input: &str) -> u32 {
     visited_locations.len() as u32
 }
 
-pub fn part2(_input: &str) -> u32 {
-    unimplemented!()
+pub fn part2(input: &str) -> u32 {
+    let mut location = (0, 0);
+    let mut visited_locations = [location].into_iter().collect::<HashSet<_>>();
+
+    // Robo santa loop
+    for c in input.chars().step_by(2) {
+        match c {
+            '>' => {
+                location.0 += 1;
+                visited_locations.insert(location);
+            }
+            'v' => {
+                location.1 -= 1;
+                visited_locations.insert(location);
+            }
+            '<' => {
+                location.0 -= 1;
+                visited_locations.insert(location);
+            }
+            '^' => {
+                location.1 += 1;
+                visited_locations.insert(location);
+            }
+            _ => unreachable!("Invalid char"),
+        }
+    }
+
+    // Santa loop
+    let mut location = (0, 0);
+    for c in input.chars().skip(1).step_by(2) {
+        match c {
+            '>' => {
+                location.0 += 1;
+                visited_locations.insert(location);
+            }
+            'v' => {
+                location.1 -= 1;
+                visited_locations.insert(location);
+            }
+            '<' => {
+                location.0 -= 1;
+                visited_locations.insert(location);
+            }
+            '^' => {
+                location.1 += 1;
+                visited_locations.insert(location);
+            }
+            _ => unreachable!("Invalid char"),
+        }
+    }
+    visited_locations.len() as u32
 }
 
 #[cfg(test)]
@@ -40,12 +89,25 @@ mod tests {
     fn test_part_1_examples() {
         assert_eq!(part1(">"), 2);
         assert_eq!(part1("^>v<"), 4);
-        assert_eq!(part1("^v^v^v^v^v^v"), 2);
+        assert_eq!(part1("^v^v^v^v^v"), 2);
     }
 
     #[test]
     fn part_1_mark_input() {
         let input = include_str!("../input.txt");
         assert_eq!(part1(input), 2081);
+    }
+
+    #[test]
+    fn test_part_2_examples() {
+        assert_eq!(part2("^v"), 3);
+        assert_eq!(part2("^>v<"), 3);
+        assert_eq!(part2("^v^v^v^v^v"), 11);
+    }
+
+    #[test]
+    fn part_2_mark_input() {
+        let input = include_str!("../input.txt");
+        assert_eq!(part2(input), 2341);
     }
 }
