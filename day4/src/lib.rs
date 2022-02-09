6e71fc80332ccf50b857dfb6db8c25daa0cee236
Chris Rice <chris@chrisrice.io>
2022-02-09 14:45:04 -0500

    Avoid allocating hex string - big win

--- day4/src/lib.rs
+++ day4/src/lib.rs
@@ -1,23 +1,33 @@
 
-fn find_md5_starting_with(input: &str, prefix: &str) -> u32 {
+fn find_md5_starting_with(input: &str, num_zeros: u8) -> u32 {
     let mut i = 1;
-    loop {
+    let num_zero_bytes = num_zeros / 2;
+    
+    'outer: loop {
         let input = format!("{input}{i}");
-        let hash = md5::compute(&input);
-        let hash = format!("{:X}", &hash);
-        if hash.starts_with(prefix) {
-            return i;
+        let hash: [u8; 16] = md5::compute(&input).into();
+        for j in 0..num_zero_bytes {
+            if hash[j as usize] != 0 {
+                i += 1;
+                continue 'outer;
+            }
         }
-        i += 1;
+        if (num_zeros & 1) == 1 {
+            if (hash[num_zero_bytes as usize] & 0b11110000) != 0 {
+                i += 1;
+                continue 'outer;
+            }
+        }
+        return i;
     }
 }
 
 pub fn part1(input: &str) -> u32 {
-    find_md5_starting_with(input, "00000")
+    find_md5_starting_with(input, 5)
 }
 
 pub fn part2(input: &str) -> u32 {
-    find_md5_starting_with(input, "000000")
+    find_md5_starting_with(input, 6)
 }
 
 #[cfg(test)]
