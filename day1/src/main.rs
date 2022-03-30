c088aa399bf80dc3b045d3c828668788544bbc7b
Mark Lodato <mlodato517@gmail.com>
2022-03-30 16:16:56 -0400

    Initial commit for day2 (#5)
    Solve day2 part 1 with tests
    
    Solve day2 part 2. Adds tests. Want itertools
    
    Add comments to day2
    
    See https://github.com/mlodato517/aoc_2015/pull/5#discussion_r793569650
    for more details.
    
    Re-organize day2
    
    **Why?**
    I felt like `dimensions` was more of a shared helper and should go below
    both parts.
    
    See https://github.com/mlodato517/aoc_2015/pull/5#discussion_r793569826
    for more details.
    
    Use include_str in binaries.
    
    See https://github.com/mlodato517/aoc_2015/pull/5#discussion_r793570892
    for more details.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,5 @@
-use std::fs::read_to_string;
-
 fn main() {
-    let input = read_to_string("input.txt").unwrap();
-    println!("part1: {}", day1::part1(&input));
-    println!("part2: {}", day1::part2(&input));
+    let input = include_str!("../input.txt");
+    println!("part1: {}", day1::part1(input));
+    println!("part2: {}", day1::part2(input));
 }
---
+++ day2/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let input = include_str!("../input.txt");
+    println!("part1: {}", day2::part1(input));
+    println!("part2: {}", day2::part2(input));
+}
